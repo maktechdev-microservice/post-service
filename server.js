10563cde499b69fc86500e02bd9705f2cac11425
maktechdev <39600404+maktechdev@users.noreply.github.com>
2022-10-27 17:06:31 +0200

    Initital Commit

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import { randomBytes } from 'crypto';
+import bodyParser from 'body-parser';
+const app = express();
+
+app.use(bodyParser.json());
+
+const port = 4000;
+const posts = {};
+
+
+
+app.get("/posts", (req, resp) => {
+    resp.send(posts);
+}); 
+
+app.post("/posts", (req, resp) => {
+    const id = randomBytes(4).toString('hex');
+    const { title } = req.body;
+
+    posts[id] = {id, title};
+
+    resp.status(201).send(posts[id]);
+
+}); 
+
+
+app.listen(port, () => console.log(`Posts server is listening at ${port}`));
